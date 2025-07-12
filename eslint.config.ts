d1e43ffa27641c69e95e9dd6816aeb756f337da6
Andreas Stassivik <andreas@stassi.net>
2025-07-11 19:03:32 -0700

    `eslint-plugin-prettier` configured with rules: - disallow end-of-line semicolons - enforce trailing commas - prefer single quotes `'` over double quotes `"`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,4 @@
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import { fixupConfigRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
 import { type Linter } from 'eslint'
@@ -5,13 +6,18 @@ import { type Linter } from 'eslint'
 const eslintConfig: Linter.Config[] = [
   ...fixupConfigRules(
     new FlatCompat().config({
-      extends: [
-        'next',
-        'next/core-web-vitals',
-        'next/typescript',
-      ],
+      extends: ['next', 'next/core-web-vitals', 'next/typescript'],
     }),
   ),
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        { semi: false, singleQuote: true, trailingComma: 'all' },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
 ]
 
 export default eslintConfig
