91e041d681eeec183bb35064b2da654bf554a5dd
Andreas Stassivik <andreas@stassi.net>
2025-07-19 02:54:41 -0700

    `@repo/mui` internal package extraction

--- apps/emergence/app/page.tsx
+++ apps/emergence/app/page.tsx
@@ -4,8 +4,8 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import NextLink from 'next/link'
-import ProTip from '@/components/ProTip'
-import Copyright from '@/components/Copyright'
+import Copyright from '@repo/mui/components/Copyright'
+import ProTip from '@repo/mui/components/ProTip'
 
 export default function Home() {
   return (
