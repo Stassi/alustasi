a5bbd3c90f14fe00972c197597e10d38830b88cc
Andreas Stassivik <andreas@stassi.net>
2025-07-21 18:13:19 -0700

    presumption

--- packages/eslint-config/src/next.ts
+++ packages/eslint-config/src/next.ts
@@ -3,13 +3,13 @@ import pluginNext from '@next/eslint-plugin-next'
 import { reactWithoutBrowserGlobals } from './react.js'
 
 export const next: ConfigArray = config([
-  // @ts-expect-error -- rules are valid
+  // @ts-expect-error -- `rules` are presumed valid
   ...reactWithoutBrowserGlobals,
   {
     plugins: {
       '@next/next': pluginNext,
     },
-    // @ts-expect-error -- rules are valid
+    // @ts-expect-error -- `rules` are presumed valid
     rules: {
       ...pluginNext.configs.recommended.rules,
       ...pluginNext.configs['core-web-vitals'].rules,
