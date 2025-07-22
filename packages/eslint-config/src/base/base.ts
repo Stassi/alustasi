34998be30db8dd37fb131ee63910bc878e625917
Andreas Stassivik <andreas@stassi.net>
2025-07-21 17:26:16 -0700

    `onlyWarn` ESLint configuration extracted from `base`

--- packages/eslint-config/src/base/base.ts
+++ packages/eslint-config/src/base/base.ts
@@ -4,24 +4,17 @@ import {
   configs as tsConfigs,
   type ConfigArray,
 } from 'typescript-eslint'
-// @ts-expect-error -- untyped module
-import onlyWarnUntyped from 'eslint-plugin-only-warn'
 import { json } from './json.js'
+import { onlyWarn } from './onlyWarn.js'
 import { prettier } from './prettier.js'
-import { turbo, type ESLintPlugin } from './turbo.js'
-
-const onlyWarn = <ESLintPlugin>onlyWarnUntyped
+import { turbo } from './turbo.js'
 
 export const base: ConfigArray = config([
   jsConfigs.recommended,
   ...tsConfigs.recommended,
   ...json,
   ...turbo,
-  {
-    plugins: {
-      onlyWarn,
-    },
-  },
+  ...onlyWarn,
   {
     ignores: ['dist/**'],
   },
