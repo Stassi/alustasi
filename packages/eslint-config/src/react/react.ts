6bea9f4c6a9c1721d05f80e020f8182ad9af6f67
Andreas Stassivik <andreas@stassi.net>
2025-07-21 19:00:42 -0700

    named imports preferred over defaults

--- packages/eslint-config/src/react/react.ts
+++ packages/eslint-config/src/react/react.ts
@@ -1,19 +1,19 @@
 import { config as tseslintConfig, type ConfigArray } from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
-import globals from 'globals'
+import { configs } from 'eslint-plugin-react'
+import { browser, serviceworker } from 'globals'
 import { base } from '../base/base.js'
 import { hooks } from './hooks.js'
 
 function config(browserGlobals: boolean): ConfigArray {
   return tseslintConfig([
     ...base,
-    pluginReact.configs.flat.recommended!,
+    configs.flat.recommended!,
     {
       languageOptions: {
-        ...pluginReact.configs.flat.recommended!.languageOptions,
+        ...configs.flat.recommended!.languageOptions,
         globals: {
-          ...globals.serviceworker,
-          ...(browserGlobals && globals.browser),
+          ...serviceworker,
+          ...(browserGlobals && browser),
         },
       },
     },
