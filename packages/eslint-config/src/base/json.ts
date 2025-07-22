30754c826c5212756b86f1261e0835069eb5de82
Andreas Stassivik <andreas@stassi.net>
2025-07-21 19:11:00 -0700

    named import preferred over default

--- packages/eslint-config/src/base/json.ts
+++ packages/eslint-config/src/base/json.ts
@@ -1,8 +1,8 @@
-import eslintPluginJsonc from 'eslint-plugin-jsonc'
+import { configs } from 'eslint-plugin-jsonc'
 import { config, type ConfigArray } from 'typescript-eslint'
 
 export const json: ConfigArray = config([
-  ...eslintPluginJsonc.configs['flat/base'],
-  ...eslintPluginJsonc.configs['flat/recommended-with-json'],
-  ...eslintPluginJsonc.configs['flat/prettier'],
+  ...configs['flat/base'],
+  ...configs['flat/recommended-with-json'],
+  ...configs['flat/prettier'],
 ])
