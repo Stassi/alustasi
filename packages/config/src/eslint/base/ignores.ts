02411f1e3d077bcb154639a479f024127778e5e2
Andreas Stassivik <andreas@stassi.net>
2025-07-24 04:28:00 -0700

    ESLint ignores `.next` & `node_modules`

--- packages/config/src/eslint/base/ignores.ts
+++ packages/config/src/eslint/base/ignores.ts
@@ -2,6 +2,6 @@ import { config, type ConfigArray } from 'typescript-eslint'
 
 export const ignores: ConfigArray = config([
   {
-    ignores: ['dist/**'],
+    ignores: ['.next/**', 'dist/**', 'node_modules/**'],
   },
 ])
