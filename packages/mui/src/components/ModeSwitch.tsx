94cda0c1bc0ab11b23f1546a4a34e32742c90b19
Andreas Stassivik <andreas@stassi.net>
2025-07-25 18:36:36 -0700

    `ModeSwitch` adheres to standards including explicit types & shorthand conditional rendering, conditionally returns `undefined` instead of `null`

--- packages/mui/src/components/ModeSwitch.tsx
+++ packages/mui/src/components/ModeSwitch.tsx
@@ -1,40 +1,42 @@
 'use client'
-import * as React from 'react'
+import { type ReactElement } from 'react'
 import Box from '@mui/material/Box'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
-import Select from '@mui/material/Select'
+import Select, { type SelectChangeEvent } from '@mui/material/Select'
 import { useColorScheme } from '@mui/material/styles'
 
-export function ModeSwitch() {
+export function ModeSwitch(): ReactElement | undefined {
   const { mode, setMode } = useColorScheme()
-  if (!mode) {
-    return null
-  }
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        mt: 1,
-        p: 1,
-      }}
-    >
-      <FormControl>
-        <InputLabel id="mode-select-label">Theme</InputLabel>
-        <Select
-          labelId="mode-select-label"
-          id="mode-select"
-          value={mode}
-          onChange={(event) => setMode(event.target.value as typeof mode)}
-          label="Theme"
-        >
-          <MenuItem value="system">System</MenuItem>
-          <MenuItem value="light">Light</MenuItem>
-          <MenuItem value="dark">Dark</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    mode && (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          mt: 1,
+          p: 1,
+        }}
+      >
+        <FormControl>
+          <InputLabel id="mode-select-label">Theme</InputLabel>
+          <Select
+            id="mode-select"
+            label="Theme"
+            labelId="mode-select-label"
+            onChange={({ target: { value } }: SelectChangeEvent): void =>
+              setMode(value as typeof mode)
+            }
+            value={mode}
+          >
+            <MenuItem value="system">System</MenuItem>
+            <MenuItem value="light">Light</MenuItem>
+            <MenuItem value="dark">Dark</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    )
   )
 }
