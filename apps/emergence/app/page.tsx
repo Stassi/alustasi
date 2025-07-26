7aa12f42f23fb88c462defcff01f40925bd91864
Andreas Stassivik <andreas@stassi.net>
2025-07-25 18:44:26 -0700

    `Copyright` named export replaces default export

--- apps/emergence/app/page.tsx
+++ apps/emergence/app/page.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import NextLink from 'next/link'
-import Copyright from '@repo/mui/components/Copyright'
+import { Copyright } from '@repo/mui/components/Copyright'
 import { ProTip } from '@repo/mui/components/ProTip'
 
 export default function Home(): ReactElement {
--- packages/mui/src/components/Copyright.tsx
+++ packages/mui/src/components/Copyright.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Typography from '@mui/material/Typography'
 import MuiLink from '@mui/material/Link'
 
-export default function Copyright() {
+export function Copyright() {
   return (
     <Typography
       variant="body2"
