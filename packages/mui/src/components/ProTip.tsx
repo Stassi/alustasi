0905cc8ac19742e66ed366bc8a57538945bdbce7
Andreas Stassivik <andreas@stassi.net>
2025-07-25 18:52:17 -0700

    `icons/LightBulb` replaces `components/LightBulbIcon`

--- packages/mui/src/components/ProTip.tsx
+++ packages/mui/src/components/ProTip.tsx
@@ -1,12 +1,12 @@
 import { type ReactElement } from 'react'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
-import { LightBulbIcon } from './LightBulbIcon'
+import { LightBulb } from '../icons/LightBulb'
 
 export function ProTip(): ReactElement {
   return (
     <Typography sx={{ mt: 6, mb: 3, color: 'text.secondary' }}>
-      <LightBulbIcon sx={{ mr: 1, verticalAlign: 'middle' }} />
+      <LightBulb sx={{ mr: 1, verticalAlign: 'middle' }} />
       {'Pro tip: See more '}
       <Link href="https://mui.com/material-ui/getting-started/templates/">
         templates
