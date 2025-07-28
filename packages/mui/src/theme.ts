8e784ed0cd2ddaa020f661c838b949cf9c7903e1
Andreas Stassivik <andreas@stassi.net>
2025-07-28 02:03:36 -0700

    `colorSchemeSelector` parameterized in function `theme`, replacing const `theme`

--- packages/mui/src/theme.ts
+++ packages/mui/src/theme.ts
@@ -1,29 +1,39 @@
 'use client'
-import { createTheme, type Theme } from '@mui/material/styles'
+import {
+  createTheme,
+  type CssVarsThemeOptions,
+  type Theme,
+} from '@mui/material/styles'
 import { roboto } from '@repo/fonts/roboto'
 
-export const theme: Theme = createTheme({
-  colorSchemes: { light: true, dark: true },
-  cssVariables: {
-    colorSchemeSelector: 'class',
-  },
-  typography: {
-    fontFamily: roboto.style.fontFamily,
-  },
-  components: {
-    MuiAlert: {
-      styleOverrides: {
-        root: {
-          variants: [
-            {
-              props: { severity: 'info' },
-              style: {
-                backgroundColor: '#60a5fa',
+export function theme({
+  colorSchemeSelector,
+}: {
+  colorSchemeSelector: CssVarsThemeOptions['colorSchemeSelector']
+}): Theme {
+  return createTheme({
+    colorSchemes: { light: true, dark: true },
+    cssVariables: {
+      colorSchemeSelector,
+    },
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    components: {
+      MuiAlert: {
+        styleOverrides: {
+          root: {
+            variants: [
+              {
+                props: { severity: 'info' },
+                style: {
+                  backgroundColor: '#60a5fa',
+                },
               },
-            },
-          ],
+            ],
+          },
         },
       },
     },
-  },
-})
+  })
+}
