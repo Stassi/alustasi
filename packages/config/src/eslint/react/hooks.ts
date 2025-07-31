7b6621b7d9570edb2a5bf9e0a594000c76e8586a
Andreas Stassivik <andreas@stassi.net>
2025-07-31 15:08:31 -0700

    `import-x/default` false positive disabled for next line

--- packages/config/src/eslint/react/hooks.ts
+++ packages/config/src/eslint/react/hooks.ts
@@ -1,4 +1,5 @@
 import { config, type ConfigArray } from 'typescript-eslint'
+// eslint-disable-next-line import-x/default
 import hooksPlugin, { configs } from 'eslint-plugin-react-hooks'
 
 export const hooks: ConfigArray = config([
