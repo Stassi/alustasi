c5ac3584218f4bd699ddb9c55ad5f0df2f4ffa88
Andreas Stassivik <andreas@stassi.net>
2025-08-08 22:57:58 -0700

    destructuring declaration propagation

--- packages/config/src/eslint/base/typeScript/typeScript.ts
+++ packages/config/src/eslint/base/typeScript/typeScript.ts
@@ -1,13 +1,11 @@
-import {
-  config,
-  configs as tsConfigs,
-  type ConfigArray,
-} from 'typescript-eslint'
+import { config, configs, type ConfigArray } from 'typescript-eslint'
 import { importX } from './importX.js'
+
+const { strictTypeChecked, stylisticTypeChecked } = configs
 
 export const typeScript: ConfigArray = config([
   {
-    extends: [tsConfigs.strictTypeChecked, tsConfigs.stylisticTypeChecked],
+    extends: [strictTypeChecked, stylisticTypeChecked],
     ignores: ['**/*.json'],
     languageOptions: {
       parserOptions: {
