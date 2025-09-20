f1431b8660627c37c39ad287e2ae4095a228fb8e
Andreas Stassivik <andreas@stassi.net>
2025-09-19 17:04:01 -0700

    `Plugin` reusable type export replaces over-specialized `typeof` definition

--- packages/config/src/eslint/base/base.ts
+++ packages/config/src/eslint/base/base.ts
@@ -1,4 +1,4 @@
-import { type Linter } from 'eslint'
+import { type ESLint, type Linter } from 'eslint'
 import { defineConfig } from 'eslint/config'
 import { globalIgnores } from './globalIgnores.js'
 import { javaScript } from './javaScript.js'
@@ -10,6 +10,7 @@ import { turbo } from './turbo.js'
 import { typeScript } from './typeScript/typeScript.js'
 
 export type Config = Linter.Config
+export type Plugin = ESLint.Plugin
 
 export const base = defineConfig([
   ...javaScript,
