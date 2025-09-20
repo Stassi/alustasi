d96dac90e062a98376bd6e75a6e89a051caf29bd
Andreas Stassivik <andreas@stassi.net>
2025-09-20 14:53:47 -0700

    import alias removal (simplification)

--- packages/config/src/eslint/base/javaScript.ts
+++ packages/config/src/eslint/base/javaScript.ts
@@ -1,7 +1,5 @@
-import { configs as jsConfigs } from '@eslint/js'
+import { configs } from '@eslint/js'
 import { defineConfig } from 'eslint/config'
 import { type Config } from './base.js'
 
-export const javaScript = defineConfig([
-  jsConfigs.recommended,
-]) satisfies Config[]
+export const javaScript = defineConfig([configs.recommended]) satisfies Config[]
