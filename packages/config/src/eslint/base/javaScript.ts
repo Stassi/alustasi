20ff4bf3b694f7c3c21e261d46d569709d1d57e1
Andreas Stassivik <andreas@stassi.net>
2025-09-21 14:41:13 -0700

    `sort-imports` rule disabled to ensure compatibility with ESLint Perfectionist

--- packages/config/src/eslint/base/javaScript.ts
+++ packages/config/src/eslint/base/javaScript.ts
@@ -2,4 +2,11 @@ import { configs } from '@eslint/js'
 import { defineConfig } from 'eslint/config'
 import { type Config } from './base.js'
 
-export const javaScript = defineConfig([configs.recommended]) satisfies Config[]
+export const javaScript = defineConfig([
+  configs.recommended,
+  {
+    rules: {
+      'sort-imports': 'off',
+    },
+  },
+]) satisfies Config[]
