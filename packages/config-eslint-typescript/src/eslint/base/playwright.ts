7436a1b8792b5da275d2a6816bb04deda793051a
Andreas Stassivik <andreas@stassi.net>
2025-10-10 13:24:34 -0700

    `eslint-plugin-playwright` configuration implemented

---
+++ packages/config-eslint-typescript/src/eslint/base/playwright.ts
@@ -0,0 +1,26 @@
+import playwrightPlugin from 'eslint-plugin-playwright'
+import { defineConfig } from 'eslint/config'
+
+import { type Config } from './base.js'
+
+const {
+    configs: { 'flat/recommended': recommended },
+  } = playwrightPlugin,
+  { rules }: Config = recommended,
+  config = defineConfig([
+    {
+      ...recommended,
+      files: ['**/*.spec.ts?(x)'],
+      rules: {
+        ...rules,
+        'playwright/max-expects': [
+          'error',
+          {
+            max: 1,
+          },
+        ],
+      },
+    },
+  ]) satisfies Config[]
+
+export const playwright: Config[] = config
