215ddd2a3cac1b281dbcdaa964322c9725f4c07d
Andreas Stassivik <andreas@stassi.net>
2025-10-12 18:17:01 -0700

    camelCase filename replaces dash-case filename

--- packages/config-eslint-typescript/src/eslint/base/testing/testing.ts
+++ packages/config-eslint-typescript/src/eslint/base/testing/testing.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'eslint/config'
 
 import { type Config } from '../base.js'
-import { jestDOM } from './jest-dom.js'
 import { jest } from './jest.js'
+import { jestDOM } from './jestDOM.js'
 import { playwright } from './playwright.js'
 
 const config = defineConfig([
