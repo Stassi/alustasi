f53250b45076d62cf6bebf28f6cda27345bc90b3
Andreas Stassivik <andreas@stassi.net>
2025-10-12 18:09:02 -0700

    redundant explicit type removal

--- packages/config-eslint-typescript/src/eslint/base/testing/jest.ts
+++ packages/config-eslint-typescript/src/eslint/base/testing/jest.ts
@@ -3,11 +3,7 @@ import { defineConfig } from 'eslint/config'
 
 import { type Config } from '../base.js'
 
-const {
-  'flat/recommended': recommended,
-  'flat/style': style,
-}: Record<`flat/${'all' | 'recommended' | 'style'}`, Config> = configs
-
+const { 'flat/recommended': recommended, 'flat/style': style } = configs
 const config = defineConfig([
   {
     files: ['**/*.test.ts?(x)'],
