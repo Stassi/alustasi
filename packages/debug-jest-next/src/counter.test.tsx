004c53fb7c5701658ed5c032cdcb0c0714fd22ad
Andreas Stassivik <andreas@stassi.net>
2025-10-17 21:24:55 -0700

    `Counter` component cloned to `@repo/debug-jest-next` from `demo-jest` app

---
+++ packages/debug-jest-next/src/counter.test.tsx
@@ -0,0 +1,18 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+
+import Counter from './counter.js'
+
+describe('Counter', (): void => {
+  beforeEach((): void => {
+    render(<Counter />)
+  })
+
+  it('should initialize at value 0', (): void => {
+    expect(screen.getByRole('heading')).toHaveTextContent('0')
+  })
+
+  it('should increment to value 1 when button is clicked', (): void => {
+    fireEvent.click(screen.getByRole('button'))
+    expect(screen.getByRole('heading')).toHaveTextContent('1')
+  })
+})
