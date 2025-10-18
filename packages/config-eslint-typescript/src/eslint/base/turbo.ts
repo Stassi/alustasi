23d399659771ee0fb6ed59dc4f61469747458605
Andreas Stassivik <andreas@stassi.net>
2025-10-18 16:16:13 -0700

    `Plugin` type casting

--- packages/config-eslint-typescript/src/eslint/base/turbo.ts
+++ packages/config-eslint-typescript/src/eslint/base/turbo.ts
@@ -1,12 +1,12 @@
 import turboPlugin from 'eslint-plugin-turbo'
 import { defineConfig } from 'eslint/config'
 
-import { type Config } from './base.js'
+import { type Config, type Plugin } from './base.js'
 
 const config = defineConfig([
   {
     plugins: {
-      turbo: turboPlugin,
+      turbo: turboPlugin as Plugin,
     },
     rules: {
       'turbo/no-undeclared-env-vars': 'warn',
