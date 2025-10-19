d7b4a51c304933db76f245bb4ccddab8f3669fd5
Andreas Stassivik <andreas@stassi.net>
2025-10-18 18:04:04 -0700

    `addCurried` test

--- packages/arithmetic/src/add.test.ts
+++ packages/arithmetic/src/add.test.ts
@@ -1,7 +1,17 @@
-import { add } from './add.js'
+import { add, addCurried } from './add.js'
 
 describe('Addition', (): void => {
+  const a = 1,
+    b = 2,
+    result = 3
+
   it('should add 1 + 2 to equal 3', (): void => {
-    expect(add(1, 2)).toBe(3)
+    expect(add(a, b)).toBe(result)
+  })
+
+  describe('curried', (): void => {
+    it('should add 1 + 2 to equal 3', (): void => {
+      expect(addCurried(a)(b)).toBe(result)
+    })
   })
 })
