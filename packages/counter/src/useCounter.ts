6c565bc30000c586cddc2a74cc64b310f12e4f00
Andreas Stassivik <andreas@stassi.net>
2025-10-20 19:46:52 -0700

    `UseCounter` type export

--- packages/counter/src/useCounter.ts
+++ packages/counter/src/useCounter.ts
@@ -3,10 +3,12 @@
 import { increment } from '@repo/arithmetic/increment'
 import { type Dispatch, type SetStateAction, useState } from 'react'
 
-export function useCounter(initialState: number): {
+export type UseCounter = {
   count: number
   increment: () => void
-} {
+}
+
+export function useCounter(initialState: number): UseCounter {
   const [count, setCount]: [number, Dispatch<SetStateAction<number>>] =
     useState(initialState)
 
