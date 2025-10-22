21d446e2ac15b9aa8e927ae462ef82f70f4061fc
Andreas Stassivik <andreas@stassi.net>
2025-10-21 22:55:01 -0700

    `useCounter` test

---
+++ packages/counter/src/useCounter.test.ts
@@ -0,0 +1,31 @@
+import { act, renderHook } from '@testing-library/react'
+
+import { repeatCurried as repeat } from './repeat'
+import { type UseCounter, useCounter } from './useCounter'
+
+describe('useCounter', (): void => {
+  it.each([
+    { clicks: 3, expected: 1, start: -2 },
+    { clicks: 2, expected: 2, start: 0 },
+    { clicks: 4, expected: 5, start: 1 },
+    { clicks: 0, expected: 3, start: 3 },
+  ])(
+    'increment $clicks clicks from $start to $expected',
+    ({
+      clicks,
+      expected,
+      start,
+    }: Record<'clicks' | 'expected' | 'start', number>): void => {
+      const { result } = renderHook((): UseCounter => useCounter(start)),
+        repeatIncrement: (n: number) => void = repeat((): void => {
+          result.current.increment()
+        })
+
+      act((): void => {
+        repeatIncrement(clicks)
+      })
+
+      expect(result.current.count).toBe(expected)
+    },
+  )
+})
