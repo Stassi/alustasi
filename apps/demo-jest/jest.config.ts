149c6a588fe4e8f1faf55a9f70a561ae0eb54d7d
Andreas Stassivik <andreas@stassi.net>
2025-10-23 14:33:35 -0700

    import `nextApp` centralized Jest configuration in package `demo-jest`

--- apps/demo-jest/jest.config.ts
+++ apps/demo-jest/jest.config.ts
@@ -1,9 +1,1 @@
-import { type Config } from '@jest/types'
-import nextJest from 'next/jest.js'
-
-export default nextJest({
-  dir: './',
-})({
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-} satisfies Config.InitialProjectOptions)
+export { nextApp as default } from '@repo/config-jest/nextApp'
