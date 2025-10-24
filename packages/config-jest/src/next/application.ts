3b9a4d62490412f696069c1a2ad6b680d43c878b
Andreas Stassivik <andreas@stassi.net>
2025-10-23 21:48:20 -0700

    superfluous modules removal (replaced by unified `next` Jest configuration)

--- packages/config-jest/src/next/application.ts
+++
@@ -1,9 +0,0 @@
-import { type Config } from '@jest/types'
-import nextJest from 'next/jest.js'
-
-export const application = nextJest({
-  dir: './',
-})({
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-} satisfies Config.InitialProjectOptions)
