203949871d1e49bcda37518d2225c4fe189ea09e
Andreas Stassivik <andreas@stassi.net>
2025-10-26 16:58:47 -0700

    `add[Curried]` input types extended to include `BigInt`

--- packages/arithmetic/src/add.ts
+++ packages/arithmetic/src/add.ts
@@ -1,7 +1,19 @@
-export function add(a: number, b: number): number {
-  return a + b
+export type BigIntCallback = NumericCallback<bigint>
+export type NumberCallback = NumericCallback<number>
+export type Numeric = bigint | number
+export type NumericCallback<T extends Numeric> = (n: T) => T
+
+export function add(a: number, b: number): number
+export function add(a: bigint, b: bigint): bigint
+export function add(a: Numeric, b: Numeric): Numeric {
+  if (typeof a === 'number' && typeof b === 'number') return a + b
+  if (typeof a === 'bigint' && typeof b === 'bigint') return a + b
+  throw new TypeError('Addition parameters must be of the same type')
 }
 
-export function addCurried(a: number) {
-  return (b: number): number => add(a, b)
+export function addCurried(a: number): NumberCallback
+export function addCurried(a: bigint): BigIntCallback
+export function addCurried(a: Numeric): BigIntCallback | NumberCallback {
+  if (typeof a === 'bigint') return (b: bigint): bigint => add(a, b)
+  else return (b: number): number => add(a, b)
 }
