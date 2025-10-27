38b2d50c4317a781d3bf2aafd107af7a75b5dce0
Andreas Stassivik <andreas@stassi.net>
2025-10-27 13:40:22 -0700

    numeric separator realignment

--- packages/hashing/src/fnv1a.ts
+++ packages/hashing/src/fnv1a.ts
@@ -2,7 +2,7 @@ import { bigInt64 } from '@repo/64-bit/bigInt'
 
 // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
 export function fnv1a64(bytes: Uint8Array): bigint {
-  let hash = 0xc_bf29_ce48_4222_325n
+  let hash = 0xcbf2_9ce4_8422_2325n
   bytes.forEach((byte: number): void => {
     hash ^= bigInt64(byte)
     hash *= bigInt64(0x1000_0000_1b3n)
