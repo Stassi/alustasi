88191ff85420c2203732862bc29bf8496bf45e80
Andreas Stassivik <andreas@stassi.net>
2025-10-28 21:29:57 -0700

    `fnv1a64` implements `multiply`

--- packages/hashing/src/fnv1a.ts
+++ packages/hashing/src/fnv1a.ts
@@ -1,11 +1,12 @@
 import { bigInt64 } from '@repo/64-bit/bigInt'
+import { multiply } from '@repo/arithmetic/multiply'
 
 // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
 export function fnv1a64(bytes: Uint8Array): bigint {
   let hash = 0xcbf2_9ce4_8422_2325n
   bytes.forEach((byte: number): void => {
     hash ^= BigInt(byte)
-    hash *= bigInt64(0x0100_0000_01b3n)
+    hash = multiply(hash, bigInt64(0x0100_0000_01b3n))
   })
   return hash
 }
