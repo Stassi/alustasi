b9d30e8b62ca3eec550898050b7890ef15d6be9c
Andreas Stassivik <andreas@stassi.net>
2025-10-28 21:07:30 -0700

    `addCurried` extraction from `add` module enforces separation of concerns

---
+++ packages/arithmetic/src/addCurried.ts
@@ -0,0 +1,13 @@
+import {
+  type BigIntCallback,
+  type NumberCallback,
+  type Numeric,
+  add,
+} from './add'
+
+export function addCurried(a: number): NumberCallback
+export function addCurried(a: bigint): BigIntCallback
+export function addCurried(a: Numeric): BigIntCallback | NumberCallback {
+  if (typeof a === 'bigint') return (b: bigint): bigint => add(a, b)
+  else return (b: number): number => add(a, b)
+}
