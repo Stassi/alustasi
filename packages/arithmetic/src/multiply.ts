62fb62edd0091a97cf5ab1ad3235b3b91176a6c6
Andreas Stassivik <andreas@stassi.net>
2025-10-28 21:27:39 -0700

    `multiply` implementation

---
+++ packages/arithmetic/src/multiply.ts
@@ -0,0 +1,9 @@
+import { type Numeric } from './add'
+
+export function multiply(a: number, b: number): number
+export function multiply(a: bigint, b: bigint): bigint
+export function multiply(a: Numeric, b: Numeric): Numeric {
+  if (typeof a === 'number' && typeof b === 'number') return a * b
+  if (typeof a === 'bigint' && typeof b === 'bigint') return a * b
+  throw new TypeError('Multiplication parameters must be of the same type')
+}
