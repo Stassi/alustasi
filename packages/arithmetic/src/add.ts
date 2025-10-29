65000f0585cc5761cbb75c9ac2545cc9ea4681ff
Andreas Stassivik <andreas@stassi.net>
2025-10-28 21:10:35 -0700

    superfluous `addCurried` definitions removal (definitions replaced or split off via module extraction)

--- packages/arithmetic/src/add.ts
+++ packages/arithmetic/src/add.ts
@@ -10,10 +10,3 @@ export function add(a: Numeric, b: Numeric): Numeric {
   if (typeof a === 'bigint' && typeof b === 'bigint') return a + b
   throw new TypeError('Addition parameters must be of the same type')
 }
-
-export function addCurried(a: number): NumberCallback
-export function addCurried(a: bigint): BigIntCallback
-export function addCurried(a: Numeric): BigIntCallback | NumberCallback {
-  if (typeof a === 'bigint') return (b: bigint): bigint => add(a, b)
-  else return (b: number): number => add(a, b)
-}
