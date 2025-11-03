ce1a4ba25a9c9b35d05815b3687cd3c45e344f52
Andreas Stassivik <andreas@stassi.net>
2025-11-03 01:56:48 -0800

    `xorMultiplyByFNVPrime64` integrated by `fnv1a64`

--- packages/hashing/src/fnv1a.ts
+++ packages/hashing/src/fnv1a.ts
@@ -1,12 +1,10 @@
-import { uInt64 } from '@repo/64-bit/uInt'
-import { multiply } from '@repo/arithmetic/multiplication/multiply'
+import { xorMultiplyByFNVPrime64 } from './xorMultiplyByFNVPrime'
 
 // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
 export function fnv1a64(bytes: Uint8Array): bigint {
-  let hash = 0xcbf2_9ce4_8422_2325n
-  bytes.forEach((byte: number): void => {
-    hash ^= BigInt(byte)
-    hash = multiply(hash, uInt64(0x0100_0000_01b3n))
-  })
-  return hash
+  return bytes.reduce(
+    (hash: bigint, byte: number): bigint =>
+      xorMultiplyByFNVPrime64(hash, BigInt(byte)),
+    0xcbf2_9ce4_8422_2325n,
+  )
 }
