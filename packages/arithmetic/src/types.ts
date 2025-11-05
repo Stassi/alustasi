456e232ca7ee850fc8d397d67f34397b848adada
Andreas Stassivik <andreas@stassi.net>
2025-11-05 12:01:26 -0800

    unused local types removal (replaced upstream by centralized `@repo/types`)

--- packages/arithmetic/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type BigIntCallback = NumericCallback<bigint>
-export type NumberCallback = NumericCallback<number>
-export type Numeric = bigint | number
-export type NumericCallback<T extends Numeric> = (n: T) => T
