ff9437a90bc9f87d79709359e908b4c1cfb8ce62
Andreas Stassivik <andreas@stassi.net>
2025-11-05 12:00:32 -0800

    imported `@repo/types` replace local types in `addWeylIncrement64`

--- packages/split-mix/src/addWeylIncrement.ts
+++ packages/split-mix/src/addWeylIncrement.ts
@@ -1,7 +1,7 @@
 import { uInt64 } from '@repo/64-bit/uInt'
 import { addCurried as add } from '@repo/arithmetic/addition/addCurried'
-import { type BigIntCallback } from '@repo/arithmetic/types'
 import { pipe } from '@repo/composition/pipe'
+import { type BigIntCallback } from '@repo/types/NumericCallback'
 
 export const addWeylIncrement64: BigIntCallback = pipe([
   add(0x9e37_79b9_7f4a_7c15n),
