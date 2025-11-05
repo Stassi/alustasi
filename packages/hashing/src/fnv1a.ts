b3ca726208243fdd6607022935a976b1a5f930c9
Andreas Stassivik <andreas@stassi.net>
2025-11-04 19:47:53 -0800

    `xorMultiplyByFNVPrime64` merged & inlined to `fnv1a64` via `Array.from`

--- packages/hashing/src/fnv1a.ts
+++ packages/hashing/src/fnv1a.ts
@@ -1,10 +1,12 @@
-import { xorMultiplyByFNVPrime64 } from './xorMultiplyByFNVPrime'
+import { uInt64 } from '@repo/64-bit/uInt'
+import { multiplyCurried as multiply } from '@repo/arithmetic/multiplication/multiplyCurried'
+import { xor } from '@repo/bitwise/xor'
+import { pipe } from '@repo/composition/pipe'
 
 // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
 export function fnv1a64(bytes: Uint8Array): bigint {
-  return bytes.reduce(
-    (hash: bigint, byte: number): bigint =>
-      xorMultiplyByFNVPrime64(hash, BigInt(byte)),
+  return Array.from(bytes, BigInt).reduce(
+    pipe([xor<bigint>, multiply(0x0100_0000_01b3n), uInt64]),
     0xcbf2_9ce4_8422_2325n,
   )
 }
