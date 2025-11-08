dca6439b4c86088280b4f68b1bb9273c6ff8caaf
Andreas Stassivik <andreas@stassi.net>
2025-11-08 11:09:06 -0800

    `@repo/fixed-width` references replace all erroneous `@repo/8-bit` & `@repo/64-bit` references in `@repo/hashing`

--- packages/hashing/src/fnv1a.ts
+++ packages/hashing/src/fnv1a.ts
@@ -1,8 +1,8 @@
-import { uInt64 } from '@repo/64-bit/uInt'
-import { uInt8s } from '@repo/8-bit/uInt8s/uInt8s'
 import { multiplyCurried as multiply } from '@repo/arithmetic/multiplication/multiplyCurried'
 import { xor } from '@repo/bitwise/xor'
 import { pipe } from '@repo/composition/pipe'
+import { uInt64 } from '@repo/fixed-width/bits64/uInt'
+import { uInt8s } from '@repo/fixed-width/bits8/uInt8s/uInt8s'
 import { type BytesLike } from '@repo/types/BytesLike'
 
 // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
