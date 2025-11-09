428963bd3dc5e79b1f18a0ea5e07a65d9d27060e
Andreas Stassivik <andreas@stassi.net>
2025-11-08 19:40:51 -0800

    split `map` in `result` array chain, improving performance by prepending additional composition step upstream of reducer

--- packages/split-mix/src/evolve.ts
+++ packages/split-mix/src/evolve.ts
@@ -4,10 +4,12 @@ import { shiftRightCurried as shiftRightBy } from '@repo/bitwise/shiftRightCurri
 import { xorCurried as xor } from '@repo/bitwise/xorCurried'
 import { pipe } from '@repo/composition/pipe'
 import { uInt64 } from '@repo/fixed-width/bits64/uInt'
+import { type BigIntCallback } from '@repo/types/NumericCallback'
 
 import { snapshot } from './snapshot'
 import { type SplitMix64 } from './splitMix'
 
+// noinspection JSUnusedGlobalSymbols
 export const evolve: (state: bigint) => SplitMix64<bigint> = pipe([
   add(0x9e37_79b9_7f4a_7c15n),
   uInt64,
@@ -20,13 +22,17 @@ export const evolve: (state: bigint) => SplitMix64<bigint> = pipe([
           { multiplier: 1n, shiftRightAmount: 31n },
         ] as const
       )
-        .map(({ multiplier, shiftRightAmount }) => ({
-          multiply: multiplyBy(multiplier),
+        .map(({ shiftRightAmount, ...props }) => ({
           shiftRightXor: pipe([shiftRightBy(shiftRightAmount), xor]),
+          ...props,
+        }))
+        .map(({ multiplier, shiftRightXor }) => ({
+          shiftRightXorMultiply64: (prevState: bigint): BigIntCallback =>
+            pipe([shiftRightXor(prevState), multiplyBy(multiplier), uInt64]),
         }))
         .reduce(
-          (prevState: bigint, { multiply, shiftRightXor }): bigint =>
-            pipe([shiftRightXor(prevState), multiply, uInt64])(prevState),
+          (prevState: bigint, { shiftRightXorMultiply64 }): bigint =>
+            shiftRightXorMultiply64(prevState)(prevState),
           state,
         ),
       state,
