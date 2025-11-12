2c980f53921b846f2c05c59f38f2f23588dc1140
Andreas Stassivik <andreas@stassi.net>
2025-11-12 14:33:11 -0800

    generalize TypeError in `add` & `multiply`

--- packages/arithmetic/src/addition/add.ts
+++ packages/arithmetic/src/addition/add.ts
@@ -3,5 +3,5 @@ import { type Numeric } from '@repo/types/Numeric'
 export function add(a: Numeric, b: Numeric): Numeric {
   if (typeof a === 'number' && typeof b === 'number') return a + b
   if (typeof a === 'bigint' && typeof b === 'bigint') return a + b
-  throw new TypeError('Addition parameters must be of the same type')
+  throw new TypeError('Parameters must be of the same type')
 }
