5b81ac23dbda840a189777d345100c1b172e4ea7
Andreas Stassivik <andreas@stassi.net>
2025-11-12 11:07:48 -0800

    type implementations:  - NumericCallbackBinary  - NumericCallbackCurried

--- packages/types/src/NumericCallback.ts
+++ packages/types/src/NumericCallback.ts
@@ -1,6 +1,10 @@
-import { type Callback } from './Callback'
+import { type Callback, type CallbackBinary } from './Callback'
 import { type Numeric } from './Numeric'
 
 export type BigIntCallback = NumericCallback<bigint>
 export type NumberCallback = NumericCallback<number>
 export type NumericCallback<T extends Numeric> = Callback<T>
+export type NumericCallbackBinary<T extends Numeric> = CallbackBinary<T>
+export type NumericCallbackCurried<T extends Numeric> = (
+  x: T,
+) => NumericCallback<T>
