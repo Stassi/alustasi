c7b13e40f2d37dc6772ccf500b4a37326117d2f6
Andreas Stassivik <andreas@stassi.net>
2025-11-12 11:09:49 -0800

    - `SnapshotProps` implementation - `SplitMix64Props` removed via inlining within `splitMix64`

--- packages/split-mix/src/splitMix.ts
+++ packages/split-mix/src/splitMix.ts
@@ -2,23 +2,21 @@ import { type Seed64Input, seed64 } from '@repo/determinism/seed'
 
 import { snapshot } from './snapshot'
 
-export type SplitMix64<Result extends bigint | undefined = undefined> =
-  Readonly<{
-    next: () => SplitMix64<bigint>
-    result: Result
-    state: bigint
-  }>
+export type SnapshotProps<Result extends bigint | undefined> = Readonly<{
+  result: Result
+  state: bigint
+}>
 
-export type SplitMix64Props = Readonly<
-  {
-    seed: Seed64Input
-  } & Partial<{
-    state: bigint | undefined
-  }>
->
+export type SplitMix64<Result extends bigint | undefined = undefined> =
+  Readonly<Record<'next', () => SplitMix64<bigint>> & SnapshotProps<Result>>
 
 // https://prng.di.unimi.it/splitmix64.c
-export function splitMix64({ seed, state }: SplitMix64Props): SplitMix64 {
+export function splitMix64({
+  seed,
+  state,
+}: Readonly<
+  Partial<Record<'state', bigint | undefined>> & Record<'seed', Seed64Input>
+>): SplitMix64 {
   return snapshot({
     result: undefined,
     state: state ?? seed64(seed),
