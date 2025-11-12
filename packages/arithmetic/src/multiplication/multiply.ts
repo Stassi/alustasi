c5181b86b4ec64238c32480383fdf994b68147d1
Andreas Stassivik <andreas@stassi.net>
2025-11-12 14:31:02 -0800

    superfluous function overloading removal in `multiply`

--- packages/arithmetic/src/multiplication/multiply.ts
+++ packages/arithmetic/src/multiplication/multiply.ts
@@ -1,7 +1,5 @@
 import { type Numeric } from '@repo/types/Numeric'
 
-export function multiply(a: number, b: number): number
-export function multiply(a: bigint, b: bigint): bigint
 export function multiply(a: Numeric, b: Numeric): Numeric {
   if (typeof a === 'number' && typeof b === 'number') return a * b
   if (typeof a === 'bigint' && typeof b === 'bigint') return a * b
