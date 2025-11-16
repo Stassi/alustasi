2d20566e47e8b8b8468a9b67979c1c30775ca26b
Andreas Stassivik <andreas@stassi.net>
2025-11-16 12:09:47 -0800

    `Snapshot*` types implementation, integration, & export: - SnapshotCurried - SnapshotResult

--- packages/split-mix/src/splitMix.ts
+++ packages/split-mix/src/splitMix.ts
@@ -3,25 +3,29 @@ import { type Numeric } from '@repo/types/Numeric'
 
 import { snapshot } from './snapshot'
 
-export type SnapshotProps<Result extends bigint | undefined> = Readonly<{
+export type SnapshotCurried<Result extends SnapshotResult> = (
+  state: bigint,
+) => SplitMix64<Result>
+
+export type SnapshotProps<Result extends SnapshotResult> = Readonly<{
   result: Result
   state: bigint
 }>
 
-export type SplitMix64<Result extends bigint | undefined = undefined> =
-  Readonly<
-    {
-      jump: (steps: Numeric) => SplitMix64<bigint>
-    } & Record<'back' | 'next', () => SplitMix64<bigint>> &
-      SnapshotProps<Result>
-  >
+export type SnapshotResult = bigint | undefined
+
+export type SplitMix64<Result extends SnapshotResult = undefined> = Readonly<
+  Record<'back' | 'next', () => SplitMix64<bigint>> &
+    Record<'jump', (steps: Numeric) => SplitMix64<bigint>> &
+    SnapshotProps<Result>
+>
 
 // https://prng.di.unimi.it/splitmix64.c
 export function splitMix64({
   seed,
   state,
 }: Readonly<
-  Partial<Record<'state', bigint | undefined>> & Record<'seed', Seed64Input>
+  Partial<Record<'state', SnapshotResult>> & Record<'seed', Seed64Input>
 >): SplitMix64 {
   return snapshot({
     result: undefined,
