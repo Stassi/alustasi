fef2403e633e28e65cc79a1d96cbf346f16977e2
Andreas Stassivik <andreas@stassi.net>
2025-11-16 12:12:11 -0800

    `snapshotCurried` (new implementation) & `snapshot` implement `Snapshot(Curried|Result)`

--- packages/split-mix/src/snapshot.ts
+++ packages/split-mix/src/snapshot.ts
@@ -1,9 +1,14 @@
 import { type Numeric } from '@repo/types/Numeric'
 
-import { type SnapshotProps, type SplitMix64 } from './splitMix'
+import {
+  type SnapshotCurried,
+  type SnapshotProps,
+  type SnapshotResult,
+  type SplitMix64,
+} from './splitMix'
 import { stepBackward, stepBy, stepForward } from './step'
 
-export function snapshot<Result extends bigint | undefined>({
+export function snapshot<Result extends SnapshotResult>({
   state,
   ...rest
 }: SnapshotProps<Result>): SplitMix64<Result> {
@@ -14,4 +19,10 @@ export function snapshot<Result extends bigint | undefined>({
     state,
     ...rest,
   }
+}
+
+export function snapshotCurried<Result extends SnapshotResult>(
+  result: Result,
+): SnapshotCurried<Result> {
+  return (state: bigint): SplitMix64<Result> => snapshot({ result, state })
 }
