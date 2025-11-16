0a1aabe0ddb5737fb9e40b2e91e97642219d577a
Andreas Stassivik <andreas@stassi.net>
2025-11-16 15:17:04 -0800

    `SplitMix64Props` extraction & export simplifies `splitMix64` implementation

--- packages/split-mix/src/splitMix.ts
+++ packages/split-mix/src/splitMix.ts
@@ -20,15 +20,14 @@ export type SplitMix64<Result extends SnapshotResult = undefined> = Readonly<
     SnapshotProps<Result>
 >
 
+export type SplitMix64Props = Readonly<
+  Partial<Record<'state', SnapshotResult>> & Record<'seed', Seed64Input>
+>
+
 export type SplitMix64State = bigint
 
 // https://prng.di.unimi.it/splitmix64.c
-export function splitMix64({
-  seed,
-  state,
-}: Readonly<
-  Partial<Record<'state', SnapshotResult>> & Record<'seed', Seed64Input>
->): SplitMix64 {
+export function splitMix64({ seed, state }: SplitMix64Props): SplitMix64 {
   return snapshot({
     result: undefined,
     state: state ?? seed64(seed),
