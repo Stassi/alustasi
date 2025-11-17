3d25020a099aee1212acde88168785100ba8033e
Andreas Stassivik <andreas@stassi.net>
2025-11-17 10:35:07 -0800

    `Snapshot*` types cloned to domain-appropriate `snapshot` module from `splitMix`

--- packages/split-mix/src/snapshot.ts
+++ packages/split-mix/src/snapshot.ts
@@ -1,13 +1,19 @@
 import { type Numeric } from '@repo/types/Numeric'
 
-import {
-  type SnapshotCurried,
-  type SnapshotProps,
-  type SnapshotResult,
-  type SplitMix64,
-  type SplitMix64State,
-} from './splitMix'
+import { type SplitMix64, type SplitMix64State } from './splitMix'
 import { stepBackward, stepBy, stepForward } from './step'
+
+export type SnapshotCurried<Result extends SnapshotResult = SplitMix64State> = (
+  state: SplitMix64State,
+) => SplitMix64<Result>
+
+export type SnapshotProps<Result extends SnapshotResult = SplitMix64State> =
+  Readonly<{
+    result: Result
+    state: SplitMix64State
+  }>
+
+export type SnapshotResult = SplitMix64State | undefined
 
 export function snapshot<Result extends SnapshotResult = SplitMix64State>({
   state,
