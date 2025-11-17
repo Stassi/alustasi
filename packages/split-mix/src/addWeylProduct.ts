a7f933bf8ae13939f21971562dffd56eb47fb02a
Andreas Stassivik <andreas@stassi.net>
2025-11-16 16:10:42 -0800

    `@repo/combinatorics/pipe` replaces erroneous references in `@repo/hashing` & `@repo/split-mix`

--- packages/split-mix/src/addWeylProduct.ts
+++ packages/split-mix/src/addWeylProduct.ts
@@ -1,6 +1,6 @@
 import { addCurried as add } from '@repo/arithmetic/addition/addCurried'
 import { multiplyCurried as multiply } from '@repo/arithmetic/multiplication/multiplyCurried'
-import { pipe } from '@repo/composition/pipe'
+import { pipe } from '@repo/combinatorics/pipe/pipe'
 import { type Numeric } from '@repo/types/Numeric'
 import { type NumericCallback } from '@repo/types/NumericCallback'
 
