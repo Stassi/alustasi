db28ad7daa3bbc79ba150103ea7f8621eb61191a
Andreas Stassivik <andreas@stassi.net>
2025-11-24 08:57:38 -0800

    `snapshot` duplicates and destructively merges both `counter[64]/snapshot[64]` files

--- packages/counter/src/snapshot.ts
+++ packages/counter/src/snapshot.ts
@@ -1,9 +1,17 @@
 import { add } from '@repo/arithmetic/addition/add'
 import { pipe } from '@repo/combinatorics/pipe/pipe'
+import { uInt64 } from '@repo/fixed-width/bits64/uInt'
 import { type Numeric } from '@repo/types/Numeric'
 
 import { type Counter, type CounterState } from './counter'
-import { stepBackward, stepBy, stepForward } from './step'
+import {
+  stepBackward,
+  stepBackward64,
+  stepBy,
+  stepBy64,
+  stepForward,
+  stepForward64,
+} from './step'
 
 export type CounterSnapshotCurried = (state: CounterState) => Counter
 
@@ -21,4 +29,20 @@ export const snapshot: (stateNumeric: Numeric) => Counter = pipe([
 export const addSnapshot: (x: Numeric, y: Numeric) => Counter = pipe([
   add,
   snapshot,
+])
+
+export const snapshot64: (stateNumeric: Numeric) => Counter = pipe([
+  uInt64,
+  (state: CounterState): Counter => ({
+    back: (): Counter => stepBackward64(state),
+    jump: (steps: Numeric): Counter => stepBy64({ state, steps }),
+    next: (): Counter => stepForward64(state),
+    result: state,
+    state,
+  }),
+])
+
+export const addSnapshot64: (x: Numeric, y: Numeric) => Counter = pipe([
+  add,
+  snapshot64,
 ])
