c2c26ce8585cf22b66ebb9aada628d2a5ac8e048
Andreas Stassivik <andreas@stassi.net>
2025-11-24 09:40:46 -0800

    `react/useCounter` duplicates and destructively merges both `counter[64]/react/useCounter[64]` files

--- packages/counter/src/react/useCounter.ts
+++ packages/counter/src/react/useCounter.ts
@@ -3,7 +3,7 @@
 import { type Numeric } from '@repo/types/Numeric'
 import { type Dispatch, type SetStateAction, useState } from 'react'
 
-import { type Counter, counter } from '../../counter'
+import { type Counter, counter, counter64 } from '../counter'
 
 export type UseCounter = {
   count: bigint
@@ -15,6 +15,20 @@ export function useCounter(initialState: Numeric = 0): UseCounter {
     Counter,
     Dispatch<SetStateAction<Counter>>,
   ] = useState(counter(initialState))
+
+  return {
+    count,
+    increment(): void {
+      setCounter((previous: Counter): Counter => previous.next())
+    },
+  }
+}
+
+export function useCounter64(initialState: Numeric = 0): UseCounter {
+  const [{ result: count }, setCounter]: [
+    Counter,
+    Dispatch<SetStateAction<Counter>>,
+  ] = useState(counter64(initialState))
 
   return {
     count,
