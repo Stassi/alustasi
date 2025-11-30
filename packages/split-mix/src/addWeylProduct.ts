a5ec46e7653b567054ecedbabfa6fb02786de65b
Andreas Stassivik <andreas@stassi.net>
2025-11-29 21:42:37 -0800

    superfluous function removal (replaced upstream by `@package/weyl-sequence`)

--- packages/split-mix/src/addWeylProduct.ts
+++
@@ -1,8 +0,0 @@
-import { addCurried as add } from '@repo/arithmetic/addition/addCurried'
-import { multiplyCurried as multiply } from '@repo/arithmetic/multiplication/multiplyCurried'
-import { pipe } from '@repo/combinatorics/pipe/pipe'
-import { type Numeric } from '@repo/types/Numeric'
-import { type NumericCallback } from '@repo/types/NumericCallback'
-
-export const addWeylProduct: (steps: Numeric) => NumericCallback<Numeric> =
-  pipe([BigInt, multiply(0x9e37_79b9_7f4a_7c15n), add])
