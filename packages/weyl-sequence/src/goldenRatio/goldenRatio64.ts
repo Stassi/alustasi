2bab6b63947413b773189bb27362a1c0d075f1d9
Andreas Stassivik <andreas@stassi.net>
2025-11-29 18:57:59 -0800

    `goldenRatio64` implementation

---
+++ packages/weyl-sequence/src/goldenRatio/goldenRatio64.ts
@@ -0,0 +1,2 @@
+// https://github.com/apache/commons-rng/blob/27c5f8c481599e73c8ec7116f6786f15917c0dab/commons-rng-simple/src/main/java/org/apache/commons/rng/simple/internal/MixFunctions.java#L25-L31
+export const goldenRatio64 = 0x9e37_79b9_7f4a_7c15n
