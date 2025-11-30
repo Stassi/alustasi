d2d1527c61322a2238107c5e47c247ebba236f64
Andreas Stassivik <andreas@stassi.net>
2025-11-29 21:07:53 -0800

    erroneous `RangeError` removal

--- packages/split-mix/src/addWeylProduct.ts
+++ packages/split-mix/src/addWeylProduct.ts
@@ -5,14 +5,4 @@ import { type Numeric } from '@repo/types/Numeric'
 import { type NumericCallback } from '@repo/types/NumericCallback'
 
 export const addWeylProduct: (steps: Numeric) => NumericCallback<Numeric> =
-  pipe([
-    pipe([
-      BigInt,
-      (steps: bigint): bigint => {
-        if (steps === 0n) throw new RangeError('Steps must be non-zero')
-        return steps
-      },
-      multiply(0x9e37_79b9_7f4a_7c15n),
-    ]) as NumericCallback<Numeric>,
-    add,
-  ])
+  pipe([BigInt, multiply(0x9e37_79b9_7f4a_7c15n), add])
