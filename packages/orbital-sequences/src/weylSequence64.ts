39c41367f744c7c7f221a655009fe38e06f1d1eb
Andreas Stassivik <andreas@stassi.net>
2025-12-02 19:58:59 -0800

    canonical reference prepended to `weylSequence64`

--- packages/orbital-sequences/src/weylSequence64.ts
+++ packages/orbital-sequences/src/weylSequence64.ts
@@ -6,6 +6,7 @@ import {
   orbitalSequence,
 } from './orbitalSequence/orbitalSequence'
 
+// https://github.com/apache/commons-rng/blob/27c5f8c481599e73c8ec7116f6786f15917c0dab/commons-rng-simple/src/main/java/org/apache/commons/rng/simple/internal/MixFunctions.java#L25-L31
 export function weylSequence64(initialState: Numeric = 0n): OrbitalSequence {
   return orbitalSequence({
     increment: 0x9e37_79b9_7f4a_7c15n,
