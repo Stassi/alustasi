9abfd6916eebe8ce4464583282075a94a39175f2
Andreas Stassivik <andreas@stassi.net>
2025-12-02 20:24:38 -0800

    `weylSequence64` (orbit) replaces all references to identical-behaving import from deprecated internal package `@repo/weyl-sequence`

--- packages/split-mix/src/addGoldenRatioProduct.ts
+++ packages/split-mix/src/addGoldenRatioProduct.ts
@@ -1,7 +1,7 @@
 import { addCurried as add } from '@repo/arithmetic/addition/addCurried'
+import { weylSequence64 } from '@repo/sequences/weylSequence64'
 import { type Numeric } from '@repo/types/Numeric'
 import { type NumericCallback } from '@repo/types/NumericCallback'
-import { weylSequence64 } from '@repo/weyl-sequence/weylSequence64'
 
 export const addGoldenRatioProduct = (
   steps: Numeric,
